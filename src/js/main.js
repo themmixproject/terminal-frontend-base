158ce355a8a98a6eecfc2d1b0da78974107598aa
themmixproject <steven.devalk@gmail.com>
2023-03-29 00:41:02 +0200

    Added print function for terminal

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 import "../scss/main.scss";
+
+let terminalWindow = document.getElementById("terminal-window");
+
+function print(text) {
+    let terminalLine = document.createElement("div");
+    terminalLine.className = "terminal-line";
+    terminalLine.innerHTML += "$~:>\xa0";
+
+    let terminalLineContent = document.createElement("div");
+    terminalLineContent.className = "terminal-line-content";
+    terminalLineContent.innerHTML = text;
+
+    terminalLine.appendChild(terminalLineContent);
+    terminalWindow.appendChild(terminalLine);
+}
+
+print("hello world!");
