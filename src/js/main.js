7b1bcb041b787c6406d0fd7d7561d3723318856d
themmixproject <steven.devalk@gmail.com>
2023-03-29 22:48:35 +0200

    Changed input prefix to ">", Added utility createDiv() function for element creation, added base for readLine() function

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,39 @@ import "../scss/main.scss";
 
 let terminalWindow = document.getElementById("terminal-window");
 
+function createDiv(className, innerHTML) {
+    let element = document.createElement("div");
+    element.className = className;
+    element.innerHTML = innerHTML;
+
+    return element;
+}
+
 function print(text) {
-    let terminalLine = document.createElement("div");
-    terminalLine.className = "terminal-line";
-    terminalLine.innerHTML += "$~:>\xa0";
+    let terminalLine = createDiv("terminal-line", ">\xa0");
+    terminalLine.appendChild(createDiv("terminal-line-content", text));
 
-    let terminalLineContent = document.createElement("div");
-    terminalLineContent.className = "terminal-line-content";
-    terminalLineContent.innerHTML = text;
-
-    terminalLine.appendChild(terminalLineContent);
     terminalWindow.appendChild(terminalLine);
 }
 
-print("hello world!");
+print("Hello World!");
+
+function readLine(prompt) {
+    let terminalLine = createDiv("terminal-line", ">\xa0");
+    let terminalLineContent = createDiv("terminal-line-content", prompt);
+
+    let form = document.createElement("form");
+    form.id = "prompt-form";
+
+    let input = document.createElement("input");
+    input.id = "prompt-input";
+
+    form.appendChild(input);
+    terminalLineContent.appendChild(form);
+    terminalLine.appendChild(terminalLineContent);
+    terminalWindow.appendChild(terminalLine);
+
+    input.focus();
+}
+
+readLine("Question? :");
