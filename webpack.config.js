782e434b99f3c5f92eb54d21b8d97afdb9cdace7
themmixproject <steven.devalk@gmail.com>
2023-03-31 21:27:49 +0200

    Fixed formatting in package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,113 +2,125 @@
  * Webpack main configuration file
  */
 
-const path = require('path');
-const fs = require('fs');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const fs = require("fs");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const environment = require('./configuration/environment');
+const environment = require("./configuration/environment");
 
-const templateFiles = fs.readdirSync(environment.paths.source)
-  .filter((file) => ['.html', '.ejs'].includes(path.extname(file).toLowerCase())).map((filename) => ({
-    input: filename,
-    output: filename.replace(/\.ejs$/, '.html'),
-  }));
+const templateFiles = fs
+    .readdirSync(environment.paths.source)
+    .filter((file) =>
+        [".html", ".ejs"].includes(path.extname(file).toLowerCase())
+    )
+    .map((filename) => ({
+        input: filename,
+        output: filename.replace(/\.ejs$/, ".html"),
+    }));
 
-const htmlPluginEntries = templateFiles.map((template) => new HTMLWebpackPlugin({
-  inject: true,
-  hash: false,
-  filename: template.output,
-  template: path.resolve(environment.paths.source, template.input),
-}));
+const htmlPluginEntries = templateFiles.map(
+    (template) =>
+        new HTMLWebpackPlugin({
+            inject: true,
+            hash: false,
+            filename: template.output,
+            template: path.resolve(environment.paths.source, template.input),
+        })
+);
 
 module.exports = {
-  entry: {
-    app: path.resolve(environment.paths.source, 'js', 'main.js'),
-  },
-  output: {
-    filename: 'js/[name].js',
-    path: environment.paths.output,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.((c|sa|sc)ss)$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.(png|gif|jpe?g|svg)$/i,
-        type: 'asset',
-        parser: {
-          dataUrlCondition: {
-            maxSize: environment.limits.images,
-          },
-        },
-        generator: {
-          filename: 'images/design/[name].[hash:6][ext]',
-        },
-      },
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        type: 'asset',
-        parser: {
-          dataUrlCondition: {
-            maxSize: environment.limits.images,
-          },
-        },
-        generator: {
-          filename: 'images/design/[name].[hash:6][ext]',
-        },
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [
-      '...',
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            // Lossless optimization with custom option
-            // Feel free to experiment with options for better result for you
-            plugins: [
-              ['gifsicle', { interlaced: true }],
-              ['jpegtran', { progressive: true }],
-              ['optipng', { optimizationLevel: 5 }],
-              // Svgo configuration here https://github.com/svg/svgo#configuration
-              [
-                'svgo',
-                {
-                  plugins: [
-                    {
-                      name: 'removeViewBox',
-                      active: false,
+    entry: {
+        app: path.resolve(environment.paths.source, "js", "main.js"),
+    },
+    output: {
+        filename: "js/[name].js",
+        path: environment.paths.output,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.((c|sa|sc)ss)$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.(png|gif|jpe?g|svg)$/i,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: environment.limits.images,
                     },
-                  ],
                 },
-              ],
-            ],
-          },
-        },
-      }),
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-    }),
-    new CleanWebpackPlugin({
-      verbose: true,
-      cleanOnceBeforeBuildPatterns: ['**/*', '!stats.json'],
-    })
-  ].concat(htmlPluginEntries),
-  target: 'web',
+                generator: {
+                    filename: "images/design/[name].[hash:6][ext]",
+                },
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: environment.limits.images,
+                    },
+                },
+                generator: {
+                    filename: "images/design/[name].[hash:6][ext]",
+                },
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            "...",
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                        // Lossless optimization with custom option
+                        // Feel free to experiment with options for better result for you
+                        plugins: [
+                            ["gifsicle", { interlaced: true }],
+                            ["jpegtran", { progressive: true }],
+                            ["optipng", { optimizationLevel: 5 }],
+                            // Svgo configuration here https://github.com/svg/svgo#configuration
+                            [
+                                "svgo",
+                                {
+                                    plugins: [
+                                        {
+                                            name: "removeViewBox",
+                                            active: false,
+                                        },
+                                    ],
+                                },
+                            ],
+                        ],
+                    },
+                },
+            }),
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+        new CleanWebpackPlugin({
+            verbose: true,
+            cleanOnceBeforeBuildPatterns: ["**/*", "!stats.json"],
+        }),
+    ].concat(htmlPluginEntries),
+    target: "web",
 };
